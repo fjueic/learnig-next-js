9221fec3b4b16f060bc87be7d86951a918c840d1
rohit <rohitmalik987789@gmail.com>
2023-03-04 15:20:40 +0530

    filtered events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,34 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+import EventList from "../components/Events/Eventlist";
 const FilteredEvents = () => {
-    return <div>FilteredEvents</div>;
+    const router = useRouter();
+    if (!router.query.slug) {
+        return <p className="center">Loading...</p>;
+    }
+    const filteredData = router.query.slug;
+    const numYear = +filteredData[0];
+    const numMonth = +filteredData[1];
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return <p>Invalid Filter. Please adjust your values!</p>;
+    }
+    const events = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    });
+
+
+    return<>
+        {events.length === 0 && <p>No events found for the chosen filter!</p>}
+        {events.length > 0 && <EventList items={events} />}
+    </>
 };
 
 export default FilteredEvents;
-    
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,20 @@
+import { getAllEvents } from "@/dummy-data";
+import EventList from "../components/Events/Eventlist";
+import EventSearch from "../components/Events/EventSearch";
+import { useRouter } from "next/router";
 const AllEvents = () => {
-    return <div>All events</div>;
+    const router = useRouter();
+    const AllEvents = getAllEvents();
+    const findEventHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    };
+    return (
+        <>
+            <EventSearch onSearch={findEventHandler} />
+            <EventList items={AllEvents} />
+        </>
+    );
 };
 
 export default AllEvents;
